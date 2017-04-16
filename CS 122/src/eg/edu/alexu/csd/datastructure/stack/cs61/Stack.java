2e9974857cd30cb4405a038c5e376390ec4c77f6
mahmoudg0 <mahmoud123468.mg@gmail.com>
2017-04-16 23:53:47 +0200

    different class

--- CS 122/src/eg/edu/alexu/csd/datastructure/stack/cs61/Stack.java
+++ CS 122/src/eg/edu/alexu/csd/datastructure/stack/cs61/Stack.java
@@ -1,36 +1,9 @@
 package eg.edu.alexu.csd.datastructure.stack.cs61;
 
-import java.util.EmptyStackException;
-
 import eg.edu.alexu.csd.datastructure.stack.IStack;
+
 public class Stack implements IStack {
 
-	private class StackNode {
-
-		private Object element;
-		private StackNode next;
-
-		public StackNode(StackNode next, Object element) {
-			this.element = element;
-			this.next = next;
-		}
-
-		public void setElement(Object element) {
-			this.element = element;
-		}
-
-		public void setNext(StackNode next) {
-			this.next = next;
-		}
-
-		public StackNode getNext() {
-			return this.next;
-		}
-
-		public Object getElement() {
-			return this.element;
-		}
-	}
 
 	private StackNode head;
 	private int size;
