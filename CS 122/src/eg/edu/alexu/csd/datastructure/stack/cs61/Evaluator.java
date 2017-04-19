08e961ff8b9436dda2c8a8c1101e34b41c5dc74e
mahmoudg0 <mahmoud123468.mg@gmail.com>
2017-04-19 18:17:24 +0200

    spaces final

--- CS 122/src/eg/edu/alexu/csd/datastructure/stack/cs61/Evaluator.java
+++ CS 122/src/eg/edu/alexu/csd/datastructure/stack/cs61/Evaluator.java
@@ -29,9 +29,9 @@ public class Evaluator implements IExpressionEvaluator{
 					temp += expression.charAt(i);
 				}
 				
-				if(i + 1 < expression.length() && expression.charAt(i+1) == ')')
-					postExpression += temp + " ";
-				else
+//				if(i + 1 < expression.length() && expression.charAt(i+1) == ')')
+//					postExpression += temp + " ";
+//				else
 					postExpression += temp + " ";
 			}
 			else
