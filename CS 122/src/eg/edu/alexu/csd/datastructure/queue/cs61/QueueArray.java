81c21057c1c036231213ea551363452bcda95e80
mahmoudg0 <mahmoud123468.mg@gmail.com>
2017-04-29 11:03:21 +0200

    Queue Array

--- CS 122/src/eg/edu/alexu/csd/datastructure/queue/cs61/QueueArray.java
+++ CS 122/src/eg/edu/alexu/csd/datastructure/queue/cs61/QueueArray.java
@@ -1,5 +1,64 @@
 package eg.edu.alexu.csd.datastructure.queue.cs61;
 
-public class QueueArray {
+import eg.edu.alexu.csd.datastructure.queue.IArrayBased;
+import eg.edu.alexu.csd.datastructure.queue.IQueue;
 
+public class QueueArray implements IQueue, IArrayBased {
+
+	private Object[] queue;
+	private int front;
+	private int rear;
+	
+	
+	public QueueArray(int size)
+	{
+		this.queue = new Object[size];
+		this.front = 0;
+		this.rear = 0;
+	}
+	
+	
+	@Override
+	public void enqueue(Object item) {
+		if(size() == this.queue.length - 1)
+			throw null;
+		if(this.rear == this.queue.length - 1)
+			this.rear = 0;
+		this.queue[this.rear] = item;
+		this.rear = (this.rear + 1) % this.queue.length;
+	}
+
+	@Override
+	public Object dequeue() {
+		if(isEmpty())
+			throw null;
+		if(this.front == this.queue.length - 1)
+			this.front = 0;
+		Object temp = this.queue[this.front];
+		this.queue[this.front] = null;
+		this.front = (this.front + 1) % this.queue.length;
+		return temp;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		if(this.front == this.rear)
+			return true;
+		return false;
+	}
+
+	@Override
+	public int size() {
+		return (this.queue.length - this.front + this.rear) % this.queue.length;
+	}
+	
+	public void print()
+	{
+		System.out.print("Queue: ");
+		for(int i = 0 ; i < this.queue.length -1 ; i++)
+		{
+			System.out.print(this.queue[i] + " ");
+		}
+		System.out.println();
+	}
 }
