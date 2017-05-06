4bdf51b626f719ffebae90026a4426ffba848b04
mahmoudg0 <mahmoud123468.mg@gmail.com>
2017-05-06 13:21:42 +0200

    interface

---
+++ CS 122/src/eg/edu/alexu/csd/datastructure/maze/cs61/MazeSolver.java
@@ -0,0 +1,23 @@
+package eg.edu.alexu.csd.datastructure.maze.cs61;
+
+import java.io.File;
+
+import eg.edu.alexu.csd.datastructure.maze.IMazeSolver;
+
+public class MazeSolver implements IMazeSolver{
+
+	@Override
+	public int[][] solveBFS(File maze) {
+		
+		return null;
+	}
+
+	@Override
+	public int[][] solveDFS(File maze) {
+		
+		return null;
+	}
+
+	
+	
+}
